3bc8a787a8e7046f6fe62d6137dea85b84ab8bc3
the evil <caiofgomesrj@gmail.com>
2022-10-03 19:45:52 -0300

    foi1

---
+++ revisão.js
@@ -0,0 +1,18 @@
+function i(){
+    var i = document.getElementById('i').value;
+    switch (i){
+        case '1':
+        alert('foi 9');
+        break;
+        case '2':
+        alert('foi 10');
+        break;
+        case '3':
+        alert('foi 11');
+        break;
+        case '4':
+        alert('foi 12');
+        break;
+        default:
+        alert("default ativado");
+    }}
